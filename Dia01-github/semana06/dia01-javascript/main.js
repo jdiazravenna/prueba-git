187e6960b315f16a4c478376f28391969463decc
Juan Diaz <jjdiazsoluciones@gmail.com>
2024-09-11 23:06:59 -0500

    semana06 dia01 javascript ejercicios con for e if

--- Dia01-github/semana06/dia01-javascript/main.js
+++ Dia01-github/semana06/dia01-javascript/main.js
@@ -183,3 +183,209 @@ console.log((montodedinero*socioporcenC)/100)
 
 
 // SOCIO A = 30%, SOCIO B = 20%, SOCIO C = 60%
+
+// Condicionales (IF)
+
+if (true){
+    // El bloque que se ejecuta si la condicion es verdadera (true)
+}
+const numero=3
+const Espar = numero % 2 === 0
+if (Espar){
+console.log('Este numero es par', numero)
+}
+// Condicionales (If, Else)
+
+if (true){
+ // se ejecuta si es verdadero
+}
+else{
+ // se ejecuta si es falsa
+}
+
+if (Espar){
+    console.log('Este numero es par', numero)
+} else{
+    console.log('Este numero es Impar', numero)
+}
+
+// Condicionales (If, Else if, else)
+
+if (true){
+    // se ejecuta este bloque si la condicion es verdadera
+} else if(true){
+    //se ejecuta si el primer if es falso
+} else if(true){
+    // se ejecuta si los anteriores son falsos
+} else{
+    // se ejecuta si ninguna condicion anterior es verdadera
+}
+
+const heroe = 'Spiderman'
+if (heroe === 'batman'){
+    console.log('Yo soy Batman')
+} else if(heroe === 'Spiderman'){
+    console.log('No me quiero ir señor Stark, no me quiero ir')
+}else if(heroe === 'Iron man'){
+    console.log('Yarvis, dame las coordenadas')
+}else {
+    console.log('No soy un héroe :(')
+}
+
+// Condicionales (Switch)
+
+const heroe2 = 'Iron Man'
+switch (heroe2){
+    case 'Batman':
+        console.log('Yo soy batman')
+        break
+    case 'Spiderman':
+        console.log('no me quiero ir señor Stark, no me quiero ir')
+        break
+    case 'Iron Man':
+        console.log('Yarvis, muestrame las coordenadas')
+        break
+    default:
+        console.log('No soy un héroe')
+}
+
+//Estructuras repetitivas
+
+//for (Sirv para repetir una o varias instrucciones)
+
+for(let i = 0; i < 10; i++){
+    console.log(i)
+}
+
+// While
+
+let j = 0
+while (j < 10){
+    console.log('while', j)
+    j = j + 1
+}
+
+// Do while
+
+let k=0
+do{
+    console.log('do while', k)
+    k = k + 1
+   }while (k < 10)
+
+// 1. Determinar si la edad de una persona es mayor o menor de edad, mostrando 'Mayor de edad' o 'menor de edad' segun corresponda
+
+const persona = 18
+if (persona >= 18){
+    console.log('Esta persona es "Mayor de edad"')
+}else{
+    console.log('Esta porsona es "menor de edad"')
+}
+
+// 2. Retornar un saludo en tres diferentes lenguajes:
+//    - si es español mostrará 'Hola'
+//    - si es inglés mostrará 'Hello'
+//    - si es aimara mostrará 'kamisaraki'
+
+const idioma = 'Ingles'
+
+if (idioma=='Español'){
+    console.log('Hola')
+}else if(idioma=='Ingles'){
+    console.log('Hello')
+}else if(idioma=='Italiano'){
+    console.log('Ciao')
+}else{
+    console.log('no se reconoce idioma')
+}
+
+// 3. Si un numero es divisible entre 3 y 5 devolver 'fizzbuzz'
+//    Si un numero es divisible entre 3 devolver 'fizz'
+//    Si un numero es divisible entre 5 devolver 'buzz'
+//    De lo contrario devolver el mismo número
+const divisible = 1
+
+if(divisible%3 ==0 && divisible%5==0){
+    console.log('fizzbuzz')
+}else if(divisible%3==0){
+    console.log('fizz')
+}else if(divisible%5==0){
+    console.log('buzz')
+}else{
+    console.log(divisible)
+}
+
+// 4. Retornar true si un numero es primo sino devolver false
+//    Pista: un numero primo es divisible por sí mismo y por 1
+
+
+
+// 5. Devolver un valor que se incremente de dos en dos y devuelva los menores a 10
+
+// Funciones (son bloques de código que podemos reutilizar)
+// Declaracion de funciones
+
+function Nombredelafuncion(){
+    // Cuerpo de la funcion y aqui va la logica a ejecutar y reutilizar
+}
+
+// Ejecutar, aqui estamos llamando a la funcion
+Nombredelafuncion()
+
+// Funciones sin parametros
+
+function ImprimirNombre(){
+    console.log('Hola soy Juan')
+    console.log('y Soy FrontEnd')
+}
+ImprimirNombre()
+
+// Funciones con parámetros y valores por defecto
+
+function Imprimirunnombre(nombre){
+    if(nombre === undefined)
+    nombre= 'anónimo'
+    console.log('Hola soy ' + nombre)
+}
+Imprimirunnombre()
+Imprimirunnombre('Juan')
+
+function imprimirNombreApellidoEdad(nombre, apellido, edad){
+   // console.log('Hola soy '+ nombre + apellido + ' y tengo ' + edad + ' años' )
+
+    console.log(`Hola soy ${nombre} ${apellido} y tengo ${edad} años`)
+}
+imprimirNombreApellidoEdad('Juan ', 'Díaz', '36')
+
+// Ejercicios
+
+// ToDo: usando las funciones si la edad de una persona es mayor de edad o menor de edad mostrando 'mayor de edad' o 'mejor de edad
+function esMayoroMenorDeEdad(edad){
+    if(edad >= 18){
+        console.log('Es mayor de edad')
+    }else{
+        console.log('Es menor de edad')
+    }
+}
+
+esMayoroMenorDeEdad(20)
+
+// FUNCIONES CON RETORNO
+
+function esMayorOMenorDeEdadConRetorno(edad) {
+    if (edad >= 18) {
+      return 'Mayor de edad'
+    } else {
+      return 'Menor de edad'
+    }
+  }
+  
+  const resultado = esMayorOMenorDeEdadConRetorno(15)
+  
+  console.log('Resultado:', resultado)
+  
+  console.log(esMayorOMenorDeEdadConRetorno(25)) // 'Mayor de edad'
+  console.log(esMayorOMenorDeEdadConRetorno(12)) // 'Mayor de edad'
+
+// Retornar true si un numero es primo si no devolver false
+// pista, un numero primo es divisible entre el mismo y entre 1
