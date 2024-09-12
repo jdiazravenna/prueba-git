f3f8732b7a1d90bd0b6046e6c759bc1f856de240
Juan Diaz <jjdiazsoluciones@gmail.com>
2024-09-11 20:08:32 -0500

    semana06 dia01 javascript js

--- Dia01-github/semana06/dia01-javascript/main.js
+++ Dia01-github/semana06/dia01-javascript/main.js
@@ -1,1 +1,43 @@
 console.log('Hola a Todos y Todes')
+
+//COMENTARIOS
+
+/*
+PARA COMENTAR VARIAS
+LINEAS A LA VEZ
+
+*/
+
+//TIPOS DE DATOS
+
+// PRIMITIVOS: Number, STRING, BOOLEAN (TRUE, FALSE), 
+// UNDEFINED, NULL, BIGINT, SYMBOL
+
+// NO PRIMITIVOS: NUMBER
+
+console.log(20)
+console.log(15.6)
+console.log(0.999)
+console.log(-35)
+
+//STRINGS
+
+console.log('Elizabeth') //string
+console.log("Minaya")//string
+console.log("Juan 'Díaz' Ravenna") //string
+
+//UNDEFINED
+let nombre //declaracion de una variable
+console.log(nombre)
+console.log(typeof nombre)
+
+//NULL
+
+let apellido = null
+console.log(apellido)
+console.log(typeof apellido)
+
+//Bigint (para guardar numeros muy grandes)
+
+//Symbol (ayudan a crear valores unicos e irrepetibles)
+
