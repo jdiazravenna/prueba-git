fa627d1e612fdd545d5939c00578ae3da3cc13d0
Juan Diaz <jjdiazsoluciones@gmail.com>
2024-09-12 22:02:34 -0500

    semana06 dia02 funciones arrays

--- Dia01-github/semana06/dia01-javascript/dia02-javascript-intermedio/main.js
+++ Dia01-github/semana06/dia01-javascript/dia02-javascript-intermedio/main.js
@@ -67,6 +67,123 @@ console.log(ListaDeNombres.at(-2))
 
 // Funcion Slice (investigar)
 
-var nombres = ["Rita", "Pedro", "Miguel", "Ana", "Vanesa"];
-var masculinos = nombres.slice(1, 2);
+let nombres = ["Rita", "Pedro", "Miguel", "Ana", "Vanesa"];
+let masculinos = nombres.slice(1, 2);
 console.log(masculinos)
+
+// Métodos de Arreglos
+
+// Metodo includes, nos indica si el valor que se pasa como parámetro se encuentra en el arreglo y siempre devuelve un boolean (True o False)
+
+const languajes = ['JavaScript', 'php', 'python', 'C#', 'C++', 'java', 'python']
+console.log(languajes.includes('java')) // true
+console.log(languajes.includes('cobol')) // false
+
+// Método Filter, nos ayuda a ubicar un elemento dentro de un arreglo, usando una condicion, y devuelve un booleano
+
+const languages = ['JavaScript', 'php', 'python', 'C#', 'C++', 'java', 'python']
+
+const resultado = languages.filter(function (language){
+  //return languaje === 'java'
+  //return languaje !== 'java'
+  return language.toLowerCase().includes('c')
+})
+console.log(resultado)
+
+// Metodp Map, evalua un arreglo y lo modifica, y hay q pasarle una función
+
+const nombresconTitulo = languages.map(function (language){
+    return '*' + language + '*'
+})
+console.log(nombresconTitulo)
+console.log(languages)
+
+
+// Sort, ordena alfabeticamente el arreglo
+languages.sort()
+console.log(languages)
+
+// Método FOREACH, nos ayuda a recorrer un arreglo sin tener ningun retorno de datos
+
+const miArreglo = []
+
+languages.forEach(function (language) {
+  miArreglo.push('El mejor: ' + language)
+})
+
+console.log(miArreglo)
+
+// Método REDUCE, nos ayuda a acumular los valores de un arreglo
+
+const numeros = [3, 40, 100, 7, 50]
+
+// Con un FOR
+let suma = 0
+for (let i = 0; i < numeros.length; i++) {
+  suma = suma + numeros[i]
+}
+console.log(suma)
+
+// Con Reduce
+
+const sumatoria = numeros.reduce(
+  function(acumulador, valorActual) {
+    return acumulador + valorActual
+  },
+  0
+)
+
+console.log(sumatoria)
+
+// OBJETOS
+
+/*
+{
+key1: value1,
+key2: value2,
+key3: value3
+}
+*/
+
+const miObjetoVacio= {}
+
+const miObjeto ={
+    nombre: 'Juan',
+    apellido: 'Díaz',
+    colorfavorito: 'azul',
+    'mi edad': 36,
+    coloresfavoritos: ['naranja', 'verde', 'rojo'],
+    cursos: [
+        {
+        id: 1,
+        nombre: 'Matematicas',
+        nota: 19
+    },
+    {   
+        id: 2,
+        nombre: 'Quimica',
+        nota: 13
+}],
+devolverCursosAprobados: function(){
+    return this.cursos.filter(function (curso){
+        return curso.nota > 13
+    })
+}
+}
+console.log(miObjetoVacio)
+console.log(miObjeto)
+console.log(miObjeto.devolverCursosAprobados())
+
+// Leer los campos de un objeto (notaacion de punto y corchete)
+
+console.log(miObjeto.nombre)
+console.log(miObjeto.apellido)
+console.log(miObjeto.edad) // undefined
+
+console.log(miObjeto['mi edad'])
+
+console.log(miObjeto.coloresfavoritos)
+console.log(miObjeto.coloresfavoritos[2])
+console.log(miObjeto.cursos)
+console.log(miObjeto.cursos[1])
+console.log(miObjeto.cursos[1].nota)
