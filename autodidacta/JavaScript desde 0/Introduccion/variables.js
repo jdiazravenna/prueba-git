5e699e06529cc8075402b2e9404c764fe94fdc08
Juan Diaz <jjdiazsoluciones@gmail.com>
2024-10-04 14:59:02 -0500

    autodidacta operadores.js

---
+++ autodidacta/JavaScript desde 0/Introduccion/variables.js
@@ -0,0 +1,23 @@
+const edad = 36
+console.log(edad)
+let nombre = 'Juan Díaz'
+console.log(nombre)
+
+let telefono, pais, id 
+telefono = '5332048'
+pais = 'Perú'
+id = '44787033'
+console.log(telefono, pais, id)
+
+const resultado = 4 + 4
+const resultado2 = resultado * 2
+console.log(resultado2)
+
+const nombre2 = 'Juan'
+const nombre3 = 'José'
+const nombrecompleto = nombre2 + nombre3
+console.log(nombrecompleto)
+
+let miVarible = 'texo'
+miVarible = 7
+console.log(miVarible)
