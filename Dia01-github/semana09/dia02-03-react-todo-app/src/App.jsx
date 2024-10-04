d7f9b275dd02f0b80671d963f213a97d01760225
Juan Diaz <jjdiazsoluciones@gmail.com>
2024-10-03 23:26:25 -0500

    semana09 dia02 todo react removertodos

--- Dia01-github/semana09/dia02-03-react-todo-app/src/App.jsx
+++ Dia01-github/semana09/dia02-03-react-todo-app/src/App.jsx
@@ -20,6 +20,66 @@ const App = () => {
   ]
 
   const [todos, setTodos] = useState(DEFAULT_TODOS)
+  const [input, setInput] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    console.log('Añadiendo una nueva tarea...')
+
+    const newTodo = {
+      id: crypto.randomUUID(),
+      title: input,
+      completed: false
+    }
+
+    setTodos([...todos, newTodo])
+
+    setInput('')
+  }
+
+  const handleChange = (event) => {
+    // Vamos a capturar lo que escribimos en la caja de texto
+    setInput(event.target.value)
+  }
+
+  const handleRemoveTodo = (event) => {
+    const id = event.target.dataset.id
+
+    const updatedTodos = todos.filter(todo => todo.id !== id)
+
+    setTodos(updatedTodos)
+  }
+
+  const handleCompleted = (event) => {
+    const { id } = event.target.dataset
+    const isChecked = event.target.checked
+
+    console.log(id, isChecked)
+
+    const updatedTodos = todos.map(todo => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed: isChecked
+        }
+      }
+
+      return todo // Deja el objeto sin modificaciones
+    })
+
+    setTodos(updatedTodos)
+  }
+ 
+  const completedTodos = todos.filter(todo => todo.completed).length
+
+  const handleRemoveTodos = (event) => {
+    const id = event.target.dataset.id
+
+    const updatedTodos = todos.filter(todo => todo.completed !== true)
+
+    setTodos(updatedTodos)
+  }
 
   return (
     <main
@@ -27,21 +87,26 @@ const App = () => {
     >
       <h1 className="text-2xl font-bold text-center">TODO APP</h1>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           className="w-full border my-3 p-2 rounded-lg"
           type="text"
           placeholder="¿Qué deseas hacer hoy?"
           required
+          onChange={handleChange}
+          value={input}
         />
       </form>
 
+      {/* {input} */}
+
       <section className="flex justify-between items-center">
         <span className="font-bold">
-          2 de 3
+          {completedTodos} de {todos.length}
         </span>
         <button
           className="bg-blue-500 text-white rounded-lg px-2 py-1 hover:bg-blue-700 duration-300"
+          onClick={handleRemoveTodos}
         >
           Limpiar completadas
         </button>
@@ -51,25 +116,31 @@ const App = () => {
         <ul className="flex flex-col gap-2">
           {todos.map(todo => {
             return (
-            <li className="flex" key={todo.id}>
-              <input
-                className="mr-2"
-                type="checkbox"
-              />
-              <div className="w-full flex justify-between items-center">
-                <span className="font-medium">
-                  {todo.title}
-                </span>
-                <button
-                  className="bg-red-300 rounded-lg px-1 py-1"
-                >
-                  ❌
-                </button>
-              </div>
-            </li>
+              <li className="flex" key={todo.id}>
+                <input
+                  className="mr-2"
+                  type="checkbox"
+                  data-id={todo.id}
+                  onChange={handleCompleted}
+                  checked={todo.completed}
+                />
+                <div className="w-full flex justify-between items-center">
+                  <span
+                    className={`font-medium ${todo.completed ? 'line-through' : '' }`}
+                  >
+                    {todo.title}
+                  </span>
+                  <button
+                    className="bg-red-300 rounded-lg px-1 py-1"
+                    data-id={todo.id}
+                    onClick={handleRemoveTodo}
+                  >
+                    ❌
+                  </button>
+                </div>
+              </li>
             )
           })}
-
 
         </ul>
       </section>
