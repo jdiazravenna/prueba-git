1e264a8bf187fbc52e288828a024016195263d11
Juan Diaz <jjdiazsoluciones@gmail.com>
2024-10-04 22:34:09 -0500

    Semana09-dia03-calculadora react

--- Dia01-github/semana09/Dia03-01-react-calculadora/src/App.jsx
+++ Dia01-github/semana09/Dia03-01-react-calculadora/src/App.jsx
@@ -1,35 +1,69 @@
+import { useState } from "react"
+
 const App = () => {
+  // TODO: Hacer la calculadora usando React.js
+  const [numeroActual, setNumeroActual] = useState('0')
+  const [operador, setOperador] = useState('')
+  const [operando, setOperando] = useState('')
+
+  const handleButton = (event) => {
+    const buttonText = event.target.textContent
+    
+    if ('+-*'.includes(buttonText)) {
+      setOperador(buttonText)
+      setOperando(Number(numeroActual))
+      setNumeroActual('0')
+    } else if (buttonText === '=') {
+      if (operador === '+') {
+        setNumeroActual(Number(operando) + Number(numeroActual))
+      } else if (operador === '-') {
+        setNumeroActual(Number(operando) - Number(numeroActual))
+      } else if (operador === '*') {
+        setNumeroActual(Number(operando) * Number(numeroActual))
+      }
+    } else if (buttonText === 'AC') {
+      setNumeroActual('0')
+      setOperador('')
+      setOperando('')
+    } else {
+      setNumeroActual(Number(numeroActual + buttonText))
+    }
+  }
+
   return (
-    <main class="bg-orange-100 w-80 mx-auto text-center mt-5 p-5">
-    <h1 class="text-3xl ">
-        Calculadora
-      </h1>
-      <section class="flex flex-col gap-4">
-        <input class="border px-3 py2 text-right text-xl"
-        type="text" 
-        readonly value="0"/>
-        <div class="grid grid-cols-3 gap-2">
-            <button class="bg-red-300 p-2 hover:bg-red-400">+</button>
-            <button class="bg-red-300 p-2 hover:bg-red-400">-</button>
-            <button class="bg-red-300 p-2 hover:bg-red-400">*</button>
+    <main className="bg-orange-100 w-80 mx-auto text-center mt-5 p-5">
+      <h1 className="text-3xl">Calculadora</h1>
 
-            <button class="bg-blue-300 p-2 hover:bg-blue-400">9</button>
-            <button class="bg-blue-300 p-2 hover:bg-blue-400">8</button>
-            <button class="bg-blue-300 p-2 hover:bg-blue-400">7</button>
+      <section className="flex flex-col gap-4">
+        <input
+          className="border px-2 py-2 text-right text-xl"
+          type="text"
+          readOnly
+          value={numeroActual}
+        />
 
-            <button class="bg-blue-300 p-2 hover:bg-blue-400">6</button>
-            <button class="bg-blue-300 p-2 hover:bg-blue-400">5</button>
-            <button class="bg-blue-300 p-2 hover:bg-blue-400">4</button>
+        <div className="grid grid-cols-3 gap-2">
+          <button onClick={handleButton} className="bg-red-300 p-2 hover:bg-red-400 duration-300">+</button>
+          <button onClick={handleButton} className="bg-red-300 p-2 hover:bg-red-400 duration-300">-</button>
+          <button onClick={handleButton} className="bg-red-300 p-2 hover:bg-red-400 duration-300">*</button>
 
-            <button class="bg-blue-300 p-2 hover:bg-blue-400">3</button>
-            <button class="bg-blue-300 p-2 hover:bg-blue-400">2</button>
-            <button class="bg-blue-300 p-2 hover:bg-blue-400">1</button>
+          <button onClick={handleButton} className="bg-blue-300 p-2 hover:bg-blue-400 duration-300">7</button>
+          <button onClick={handleButton} className="bg-blue-300 p-2 hover:bg-blue-400 duration-300">8</button>
+          <button onClick={handleButton} className="bg-blue-300 p-2 hover:bg-blue-400 duration-300">9</button>
 
-            <button class="bg-blue-300 p-2 hover:bg-blue-400">0</button>
-            <button class="bg-green-300 p-2 hover:bg-green-400">CE</button>
-            <button class="bg-green-300 p-2 hover:bg-green-400">=</button>
+          <button onClick={handleButton} className="bg-blue-300 p-2 hover:bg-blue-400 duration-300">4</button>
+          <button onClick={handleButton} className="bg-blue-300 p-2 hover:bg-blue-400 duration-300">5</button>
+          <button onClick={handleButton} className="bg-blue-300 p-2 hover:bg-blue-400 duration-300">6</button>
+
+          <button onClick={handleButton} className="bg-blue-300 p-2 hover:bg-blue-400 duration-300">1</button>
+          <button onClick={handleButton} className="bg-blue-300 p-2 hover:bg-blue-400 duration-300">2</button>
+          <button onClick={handleButton} className="bg-blue-300 p-2 hover:bg-blue-400 duration-300">3</button>
+
+          <button onClick={handleButton} className="bg-blue-300 p-2 hover:bg-blue-400 duration-300">0</button>
+          <button onClick={handleButton} className="bg-green-300 p-2 hover:bg-green-400 duration-300">AC</button>
+          <button onClick={handleButton} className="bg-green-300 p-2 hover:bg-green-400 duration-300">=</button>
         </div>
-    </section>
+      </section>
     </main>
   )
 }
