c9edbc8baadb5717477600fb21f8210ecaf4f298
Juan Diaz <jjdiazsoluciones@gmail.com>
2024-10-16 22:17:53 -0500

    Semana11-dia01-DBZfetch-routes

--- Dia01-github/Semana11/Dia01-react-router-components/src/App.jsx
+++ Dia01-github/Semana11/Dia01-react-router-components/src/App.jsx
@@ -1,20 +1,21 @@
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { HomePage } from './pages/HomePage'
 import { LoginPage } from './pages/LoginPage'
 import { RegisterPage } from './pages/RegisterPage'
+import LayoutBase from './layouts/LayoutBase'
+import CharacterPage from './pages/CharacterPage'
 
 export default function App() {
   return (
     <BrowserRouter>
-      <header className='flex gap-2 text-blue-600'>
-        <Link to='/'>Home</Link>
-        <Link to='/login'>Login</Link>
-        <Link to='/register'>Register</Link>
-      </header>
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/register' element={<RegisterPage />} />
+        <Route path='/' element={<LayoutBase />}>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/register' element={<RegisterPage />} />
+
+          <Route path='/characters/:id' element={<CharacterPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
--- Dia01-github/Semana11/Dia01-react-router-components/src/pages/HomePage.jsx
+++ Dia01-github/Semana11/Dia01-react-router-components/src/pages/HomePage.jsx
@@ -1,5 +1,12 @@
+import CharacterList from "../Components/home-page/CharacterList"
+
+
 export const HomePage = () => {
-    return (
-    <h1>HomePage</h1>
-)
+  return (
+    <>
+      <h1  className="text-3xl font-bold undeline text-center">Character List</h1>
+
+      <CharacterList/>
+    </>
+  )
 }
