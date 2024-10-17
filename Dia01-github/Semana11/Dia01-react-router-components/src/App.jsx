dea7e0c89696b6e3d5c066e555cdce2def38ac05
Juan Diaz <jjdiazsoluciones@gmail.com>
2024-10-16 20:20:14 -0500

    Semana11-dia01-app.jsx

--- Dia01-github/Semana11/Dia01-react-router-components/src/App.jsx
+++ Dia01-github/Semana11/Dia01-react-router-components/src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import { HomePage } from './pages/HomePage'
+import { LoginPage } from './pages/LoginPage'
+import { RegisterPage } from './pages/RegisterPage'
 
-const App = () => {
+export default function App() {
   return (
-    <h1 className='text-3xl font-bold underline'>Hola mundo</h1>
+    <BrowserRouter>
+      <header className='flex gap-2 text-blue-600'>
+        <Link to='/'>Home</Link>
+        <Link to='/login'>Login</Link>
+        <Link to='/register'>Register</Link>
+      </header>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
-
-export default App
