39ca6031af3a0ecde62c31293082640f5cb16885
Juan Diaz <jjdiazsoluciones@gmail.com>
2024-10-23 19:50:59 -0500

    Semana11-dia03-fetch-crud-routes terminado peliculas

--- Dia01-github/Semana11/dia03-react-peliculas-crud-fetch-router/src/pages/HomePage.jsx
+++ Dia01-github/Semana11/dia03-react-peliculas-crud-fetch-router/src/pages/HomePage.jsx
@@ -1,15 +1,50 @@
 import { useEffect, useState } from "react";
-import { TbEdit, TbTrash } from "react-icons/tb";
-
+import { TbEdit, TbTrash, TbEye } from "react-icons/tb";
+import { removePelicula } from "../services/peliculas";
 import { fetchPeliculas } from "../services/peliculas";
+import { Link } from "react-router-dom";
+import Swal from 'sweetalert2'
 
 const HomePage = () => {
   const [peliculas, setPeliculas] = useState([])
+
+  const GENEROS = {
+    1: "Comedia",
+    2: "Acción",
+    3: "Animación",
+    4: "Aventura",
+  }
 
   useEffect(() => {
     fetchPeliculas()
       .then(data => setPeliculas(data))
   }, [])
+
+  const handleRemove = (id) => {
+    console.log("Eliminando película...", id)
+
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then(async (result) => {
+      // aqui colocamos el async porq esta en otra funcion
+      // cuando el usuario presiona el boton YES
+      if (result.isConfirmed) {
+        const res = await removePelicula(id)
+
+        console.log(res)
+
+        const dataPeliculas = await fetchPeliculas()
+
+        setPeliculas(dataPeliculas)
+      }
+    })
+  }
 
   // DONE: Renderizar las peliculas en la tabla de abajo usando el estado peliculas
 
@@ -42,7 +77,7 @@ const HomePage = () => {
                     <strong>Estreno:</strong> {pelicula.estreno}
                   </div>
                   <div className="fs-small">
-                    <strong>Genero:</strong> {pelicula.generoId}
+                    <strong>Genero:</strong> {GENEROS[pelicula.generoId] ?? '-'}
                   </div>
                   <div className="fs-small">
                     <strong>Resumen:</strong> {pelicula.resumen}
@@ -50,8 +85,13 @@ const HomePage = () => {
                 </td>
                 <td>
                   <div className="flex gap-0.5">
-                    <button><TbEdit /></button>
-                    <button><TbTrash /></button>
+                    <Link to={`/ver/${pelicula.id}`}>
+                      <button><TbEye /></button>
+                    </Link>
+                    <Link to={`/editar/${pelicula.id}`}>
+                      <button><TbEdit /></button>
+                    </Link>
+                    <button onClick={() => handleRemove(pelicula.id)}><TbTrash /></button>
                   </div>
                 </td>
               </tr>
--- Dia01-github/Semana11/dia03-react-peliculas-crud-fetch-router/src/services/peliculas.js
+++ Dia01-github/Semana11/dia03-react-peliculas-crud-fetch-router/src/services/peliculas.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://67074c39a0e04071d229b837.mockapi.io/api/v1/'
+const BASE_URL = 'https://67074c39a0e04071d229b837.mockapi.io/api/v1'
 
 export const fetchPeliculas = async () => {
   const response = await fetch(`${BASE_URL}/peliculas`)
@@ -18,4 +18,38 @@ export const crearPelicula = async (form) => {
   const response = await fetch(`${BASE_URL}/peliculas`, options)
 
   return await response.json()
+}
+
+export const obtenerPelicula = async (id) => {
+  const response = await fetch(`${BASE_URL}/peliculas/${id}`)
+
+  return await response.json()
+}
+
+export const editarPelicula = async (form, id) => {
+  const url = `${BASE_URL}/peliculas/${id}`
+
+  const options = {
+    method: 'PUT',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(form)
+  }
+
+  const response = await fetch(url, options)
+
+  return await response.json()
+}
+
+export const removePelicula = async (id) => {
+  const url = `https://67074c39a0e04071d229b837.mockapi.io/api/v1/peliculas/${id}`
+
+  const options = {
+      method: 'DELETE'
+  }
+  const response = await fetch(url, options)
+
+  return await response.json()
+
 }
