711aebb60068e61ba0d12129d864cdb2a777724e
Juan Diaz <jjdiazsoluciones@gmail.com>
2024-10-25 23:15:00 -0500

    Semana12-dia03-heroes-firebase

---
+++ Dia01-github/Semana12/dia03-react-firebase-heroes-crud/src/hooks/useHeroes.js
@@ -0,0 +1,53 @@
+import { collection, query, getDocs, addDoc, doc, deleteDoc } from 'firebase/firestore'
+
+import { db } from '../services/firebase'
+
+export const useHeroes = () => {
+  const reference = collection(db, 'heroes')
+
+  const fetchHeroes = async() => {
+    const q = query(reference)
+
+    const data = await getDocs(q)
+
+    const results = []
+
+    data.forEach(doc => {
+      console.log(doc.id, doc.data())
+      results.push({
+        docId: doc.id,
+        ...doc.data() // Representa el documento actual
+      })
+    })
+
+    return results
+  }
+
+  const createHero = async (hero) => {
+    const newHero = {
+      name: hero.name,
+      image: hero.image
+    }
+
+    const response = await addDoc(reference, newHero)
+
+    return {
+      id: response.id,
+      newHero
+    }
+  }
+
+  const removeHero = async (id) => {
+    const document = doc(db, 'heroes', id )
+
+    const response = await deleteDoc(document)
+
+    return response
+  }
+
+  return {
+    fetchHeroes,
+    createHero,
+    removeHero
+  }
+}
